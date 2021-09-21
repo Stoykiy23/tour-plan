9f4728865809e26f4831d8fcd873c18fc502d56f
stoykiy23 <stoykiy23@mail.ru>
2021-09-21 16:51:16 +0300

    create: swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button__next',
+    prevEl: '.slider-button__prev',
+  },
+});
